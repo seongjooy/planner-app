092858be8a837415d1bb4a952dd4092216ecbcb3
seongjooy <seongjooyoon@gmail.com>
2022-02-17 16:56:47 +0900

    Initial Commit

---
+++ myapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myapp
+//
+//  Created by Seong Joo Yoon on 2022-02-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myapp/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  myapp
+//
+//  Created by Seong Joo Yoon on 2022-02-17.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
