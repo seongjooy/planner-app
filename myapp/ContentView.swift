6ce77417ecc40deee7a1b985ecde104b9528b0ad
seongjooy <seongjooyoon@gmail.com>
2022-02-18 12:26:29 +0900

    adding files

--- myapp/ContentView.swift
+++ myapp/ContentView.swift
@@ -1,7 +1,4 @@
 //
-//  ContentView.swift
-//  myapp
-//
 //  Created by Seong Joo Yoon on 2022-02-17.
 //
 
@@ -9,13 +6,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        HStack{
+            Image("sweden")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            VStack(alignment: .leading) {
+                Text("Swedish flag")
+                Text("Italian flag")
+                Spacer()
+
+            }
+            Button("Click Me", action: {
+                print("hello world")
+            })
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 8")
+            ContentView()
+        }
     }
 }
